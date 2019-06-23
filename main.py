12bdf94dd8caef1d8f9b74e0a6161861c4f77e75
Gowtham Behara <gothsiddu@gmail.com>
2019-06-23 12:54:26 +0530

    Use LBP cascade to detect faces

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ def detect_faces(face_cascade_detector, bgr_scale_img, scaling_factor=1.1):
     return bgr_scale_img_copy
 
 
-test_img = cv2.imread('data/test1.jpg')
+test_img = cv2.imread('data/test3.jpg')
 plt.imshow(convertBGRToRGB(test_img))
 plt.show()
 
-haar_cascade_face_detector = cv2.CascadeClassifier('venv/Lib/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
-faces_detected = detect_faces(haar_cascade_face_detector, test_img)
+#haar_cascade_face_detector = cv2.CascadeClassifier('venv/Lib/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
+lbp_cascade_face_detector = cv2.CascadeClassifier('venv/Lib/site-packages/cv2/data/lbpcascade_frontalface_improved.xml')
+faces_detected = detect_faces(lbp_cascade_face_detector, test_img)
 plt.imshow(convertBGRToRGB(faces_detected))
 plt.show()
