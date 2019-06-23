e108226bac065d4d3871cfc9cb500b83465264ae
Gowtham Behara <gothsiddu@gmail.com>
2019-06-23 12:21:20 +0530

    Load color img and convert to grayscale

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import cv2
+from matplotlib import pyplot as plt
 
 
 def convertToRGB(bgr_img):
     return cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+
+
+test1 = cv2.imread('data/test1.jpg')
+gray_img = cv2.cvtColor(test1, cv2.COLOR_BGR2GRAY)
+plt.imshow(gray_img, cmap='gray')
+plt.show()
