adac4c30a60cab9fdf9ff92ddc3851ddc78e3ae3
Gowtham Behara <gothsiddu@gmail.com>
2019-06-23 12:41:37 +0530

    Modularise code with detect_faces func

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-def convertToRGB(bgr_img):
+def convertBGRToRGB(bgr_img):
     return cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
 
 
-test1 = cv2.imread('data/test1.jpg')
-gray_img = cv2.cvtColor(test1, cv2.COLOR_BGR2GRAY)
-plt.imshow(gray_img, cmap='gray')
+def detect_faces(face_cascade_detector, bgr_scale_img, scaling_factor=1.1):
+    bgr_scale_img_copy = bgr_scale_img.copy()
+    gray_scale_img = cv2.cvtColor(bgr_scale_img_copy, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade_detector.detectMultiScale(gray_scale_img, scaleFactor=scaling_factor, minNeighbors=5)
+    for x, y, w, h in faces:
+        cv2.rectangle(bgr_scale_img_copy, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+
+    return bgr_scale_img_copy
+
+
+test_img = cv2.imread('data/test1.jpg')
+plt.imshow(convertBGRToRGB(test_img))
 plt.show()
 
 haar_cascade_face_detector = cv2.CascadeClassifier('venv/Lib/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
-faces = haar_cascade_face_detector.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
-print(len(faces))
-
-for (x, y, w, h) in faces:
-    cv2.rectangle(test1, (x, y), (x+w, y+h), color=(0, 255, 0), thickness=2)
-
-plt.imshow(convertToRGB(test1))
+faces_detected = detect_faces(haar_cascade_face_detector, test_img)
+plt.imshow(convertBGRToRGB(faces_detected))
 plt.show()
