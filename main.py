272a00af5e54c25b792fe78705691de663b5f783
Gowtham Behara <gothsiddu@gmail.com>
2019-06-23 12:27:40 +0530

    Use haar_cascade_face detector and print num of faces detected

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ test1 = cv2.imread('data/test1.jpg')
 gray_img = cv2.cvtColor(test1, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray_img, cmap='gray')
 plt.show()
+
+haar_cascade_face_detector = cv2.CascadeClassifier('venv/Lib/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
+faces = haar_cascade_face_detector.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
+print(len(faces))
+
